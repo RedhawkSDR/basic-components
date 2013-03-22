b1800c3f71a4e7f108f416a05156ea2f7328eb25
Daniel Wille <daniel.wille@axiosengineering.com>
2013-03-22 18:19:33 -0400

    Restore license

--- freqfilter/python/freqfilter_base.py
+++ freqfilter/python/freqfilter_base.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-# This file is protected by Copyright. Please refer to the COPYRIGHT file distr
+#
+# This file is protected by Copyright. Please refer to the COPYRIGHT file distributed with this 
 # source distribution.
 # 
 # This file is part of REDHAWK Basic Components.
 # 
-# REDHAWK Basic Components is free software: you can redistribute it and/or mod
-# the GNU Lesser General Public License as published by the Free Software Found
+# REDHAWK Basic Components is free software: you can redistribute it and/or modify it under the terms of 
+# the GNU Lesser General Public License as published by the Free Software Foundation, either 
 # version 3 of the License, or (at your option) any later version.
 # 
-# REDHAWK Basic Components is distributed in the hope that it will be useful, b
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICU
+# REDHAWK Basic Components is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 # PURPOSE.  See the GNU Lesser General Public License for more details.
 # 
-# You should have received a copy of the GNU Lesser General Public License alon
+# You should have received a copy of the GNU Lesser General Public License along with this 
 # program.  If not, see http://www.gnu.org/licenses/.
+#
 #
 # AUTO-GENERATED CODE.  DO NOT MODIFY!
 #
