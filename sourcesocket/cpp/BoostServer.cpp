316e74d745b3cf490a29dadd21bcd91260b396bc
Daniel Wille <daniel.wille@axiosengineering.com>
2013-07-09 19:07:04 -0400

    Add missing template declaration

--- sourcesocket/cpp/BoostServer.cpp
+++ sourcesocket/cpp/BoostServer.cpp
@@ -210,6 +210,7 @@ template void server::write(std::vector<short, _seqVector::seqVectorAllocator<sh
 template void server::write(std::vector<unsigned int, _seqVector::seqVectorAllocator<unsigned int> >&);
 template void server::write(std::vector<int, _seqVector::seqVectorAllocator<int> >&);
 template void server::write(std::vector<unsigned long, _seqVector::seqVectorAllocator<unsigned long> >&);
+template void server::write(std::vector<long, _seqVector::seqVectorAllocator<long> >&);
 template void server::write(std::vector<float, _seqVector::seqVectorAllocator<float> >&);
 template void server::write(std::vector<double, _seqVector::seqVectorAllocator<double> >&);
 
